2ae65aafc74f9f109a49f45af46cff8bd9e0d6f4
Neil Price <neil.price@thoughtworks.com>
2024-03-27 11:13:22 +0000

    Updated GA

--- tf_prerequisites/iam-github.tf
+++ tf_prerequisites/iam-github.tf
@@ -1,7 +1,7 @@
-
 locals {
   gihub_oidc_already_exists = false
 }
+
 resource "aws_iam_openid_connect_provider" "default" {
   count                       = local.gihub_oidc_already_exists ? 0 : 1
   url = "https://token.actions.githubusercontent.com"
@@ -12,34 +12,85 @@ resource "aws_iam_openid_connect_provider" "default" {
 }
 
 data "aws_caller_identity" "current" {}
-
-resource "aws_iam_policy" "iam" {
-  name = format("%s-github-deployment-policy", var.prefix)
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ec2:*Instance*",
-          "iam:PassRole",
-          # Likely to need more or different permissions for successful deployment
-          # but you want to try to use least privilege principle where possible
-        ],
-        "Resource" : "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "role_policy_attachment" {
-  name       = "Policy Attachement"
-  policy_arn = aws_iam_policy.iam.arn
-  roles      = [aws_iam_role.github_actions_role.name]
-}
+data "aws_region" "current" {}
 
 resource "aws_iam_role" "github_actions_role" {
   name = format("%s-github-actions-role", var.prefix)
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRDSFullAccess",
+  ]
+
+  inline_policy {
+    name = "extra_permissions"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:*Bucket*",
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:*",
+          ],
+          "Resource" : ["arn:aws:s3:::${var.prefix}-tfstate", "arn:aws:s3:::${var.prefix}-tfstate/*"]
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:*",
+          ],
+          "Resource" : format("arn:aws:dynamodb:%s:%s:table/%s-tfstate-locks*", data.aws_region.current.id, data.aws_caller_identity.current.id, var.prefix)
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "secretsmanager:*",
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "logs:*",
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ecr:*"
+          ],
+          "Resource" : format("arn:aws:ecr:*:%s:repository/*", data.aws_caller_identity.current.id)
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "iam:PassRole",
+            "iam:*Role*",
+            "iam:*Policy*"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ecr:GetAuthorizationToken"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
